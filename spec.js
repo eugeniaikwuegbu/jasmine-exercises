4a34b08b0b0f9f79490e2fe43277b4b58117f4cc
Eugenia Ikwuegbu <eugeniaikwuegbu@gmail.com>
2020-09-14 13:33:11 +0100

    committing changes

--- spec.js
+++ spec.js
@@ -1,10 +1,10 @@
 // TESTING FUNCTION REPLACEWITH
 
     describe("replaceWith", function(){
-    it ("to return a string with replacements", function(){
+    it ("Should return a string with replacements", function(){
     expect( replaceWith("awesome", "e", "z")).toBe("awzsomz")
     })
-    it ("is case sensitive", function(){
+    it ("should be case sensitive", function(){
     expect(replaceWith("Foo", "F", "B")).toBe("Boo")
     })
      
@@ -12,10 +12,10 @@
 //TESTION FUNCTION EXPAND
 
     describe("expand", function(){
-    it ("returns a copy of the array with as many numbers as specified", function(){
+    it ("Should return a copy of the array with as many numbers as specified", function(){
     expect(expand([1,2,3],3)).toEqual([1,2,3,1,2,3,1,2,3])
      })
-    it ("returns a copy of the array with as many numbers as specified", function(){
+    it ("Should return a copy of the array with as many numbers as specified", function(){
     expect(expand(["foo","test"],1)).toEqual(["foo","test"])
 
      })
@@ -24,14 +24,14 @@
 
 // TESTING FUNCTION ACCEPTNUMBERSONLY 
     describe("acceptNumbersOnly", function(){
-    it ("returns false if all are not numbers", function(){
+    it ("should return false if all are not numbers", function(){
     expect(acceptNumbersOnly(1,"foo")).toEqual(false)
      })
-    it ("returns true if all are numbers", function(){
+    it ("should return true if all are numbers", function(){
     expect(acceptNumbersOnly(1,2,3,4,5,6,7) ).toEqual(true)
      })
 
-    it ("returns false if NaN is included", function(){
+    it ("should return false if NaN is included", function(){
     expect(acceptNumbersOnly(1,2,3,4,5,6,NaN)).toEqual(false)
 
      })
@@ -41,7 +41,7 @@
 //TESTING FUNCTION MERGEARRAYS
 
     describe("mergeArrays", function(){
-    it (" returns one array with the values sorted", function(){
+    it ("should return one array with the values sorted", function(){
     expect(mergeArrays([2,1],[3,4])).toEqual([1,2,3,4])
      })
      }) 
@@ -49,7 +49,7 @@
 //TESTING FUNCTION MERGEOBJECTS
 
     describe("mergeObjects", function(){
-    it ("returns an object with the keys and values combined", function(){
+    it ("should return an object with the keys and values combined", function(){
     expect(mergeObjects(obj1, obj2)).toEqual(
         {   
             name: "Foo",
